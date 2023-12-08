5c5e6323659dd5ec1cc0bc14abf21c8447456278
h3cth0r <hectorsucre13@gmail.com>
2023-12-08 15:28:23 -0600

    added weighted moving average. Must check memory management

--- statisco/c_src/indicators/MAs.c
+++ statisco/c_src/indicators/MAs.c
@@ -152,9 +152,54 @@ PyObject *EMA(PyObject *self, PyObject *args) {
 }
 
 
+PyObject *WMA(PyObject *self, PyObject *args) {
+    PyObject *input_array;
+    npy_int32 window_t;
+
+    if (!PyArg_ParseTuple(args, "Oi", &input_array, &window_t) || PyErr_Occurred()) {
+        PyErr_SetString(PyExc_TypeError, "Invalid argument. Expected a numpy array and an int.");
+        return NULL;
+    }
+    if (window_t <= 0) {
+        PyErr_SetString(PyExc_ValueError, "Window size must be a positive integer.");
+        return NULL;
+    }
+    PyArrayObject *arr = (PyArrayObject *)PyArray_FROM_OTF(input_array, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    if (arr == NULL) {
+        return NULL;
+    }
+
+    double *data = (double *)PyArray_DATA(arr);
+    npy_intp size = PyArray_SIZE(arr);
+
+    PyObject *result = PyArray_NewLikeArray(arr, NPY_CORDER, NULL, 0);
+    if (result == NULL) {
+        Py_XDECREF(arr);
+        return NULL;
+    }
+    double *result_data = (double *)PyArray_DATA((PyArrayObject *)result);
+
+    double sum = 0;
+    double counter = 0;
+
+    for (npy_intp i = window_t - 1; i < size; i++) {
+        sum = 0;
+        counter = 1;
+        for (npy_intp j = i - window_t + 1; j <= i; j++) {
+            sum += data[j] * (counter / (window_t * (window_t + 1) / 2));
+            counter += 1;
+        }
+        result_data[i] = sum;
+    }
+
+    return result;
+}
+
+
 PyMethodDef methods[] = {
   {"SMA",        (PyCFunction)SMA,          METH_VARARGS, "Computes the Simple Moving Average."},
-  {"EMA",        (PyCFunction)EMA,          METH_VARARGS, "Computes the Weighted Moving Average."},
+  {"EMA",        (PyCFunction)EMA,          METH_VARARGS, "Computes the Exponential Moving Average."},
+  {"WMA",        (PyCFunction)WMA,          METH_VARARGS, "Computes the Weighted Moving Average."},
   {NULL, NULL, 0, NULL}
 };
 
